f4edaa7459cb4ec6964fabd6c0797a74ba87c036
Drew Pate <andrewcpate@gmail.com>
2021-09-30 11:23:25 -0400

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+function App(){
+  const [data, setData] = React.useState(null);        
+  const [loaded, setLoaded] = React.useState(false);
+
+  React.useEffect(() => {
+      async function getData() {
+          const response = await fetch('./books.json');
+          const json     = await response.json();
+          setData(json);
+          setLoaded(true);
+      }
+      getData();
+  },[])
+  console.log('loaded:', loaded, 'data:', data);
+
+  return (<>
+      <div className="container">
+          <h1>React Components</h1>
+          {loaded && data.books.map((book,i) => 
+              <mit-book key={i}
+                  title={book.title} 
+                  subtitle={book.subtitle}
+                  author={book.author}
+                  publisher={book.publisher}
+                  description={book.description}
+              />
+          )}
+      </div>        
+  </>);   
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App/>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
